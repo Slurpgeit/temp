54ad10565cbe52a2475a4579e81f58e531137106
Slurpgeit <d.maasland@gmail.com>
2019-04-13 15:19:06 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -5,6 +5,7 @@ $wc = New-Object System.Net.WebClient
 # URLs
 $SysInternals = "https://download.sysinternals.com/files/SysinternalsSuite.zip"
 $hashcat = "https://github.com/Slurpgeit/temp/raw/master/hashcat-5.1.0.zip"
+$Geth = "https://gethstore.blob.core.windows.net/builds/geth-windows-amd64-1.8.26-cdae1c59.exe"
 
 # Functions
 function Unzip($file, $destination) {
@@ -21,3 +22,5 @@ Unzip -File "c:\temp\sysinternals.zip" -Destination "c:\temp"
 
 $wc.DownloadFile($hashcat,"c:\temp\hashcat.zip")
 Unzip -File "c:\temp\hashcat.zip" -Destination "c:\temp"
+
+$wc.DownloadFile($Geth,"c:\temp\geth.exe")
